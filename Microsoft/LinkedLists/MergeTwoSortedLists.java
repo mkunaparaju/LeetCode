373c46b6caba4f8edb21ce051c096b3560050775
Kunaparaju, Manasa <mkunaparaju@gmail.com>
2018-12-17 15:12:15 -0700

    update

---
+++ Microsoft/LinkedLists/MergeTwoSortedLists.java
@@ -0,0 +1,4 @@
+package Microsoft.LinkedLists;
+//https://leetcode.com/explore/interview/card/microsoft/32/linked-list/175/
+public class MergeTwoSortedLists {
+}
---
+++ Microsoft/LinkedLists/ReverseLinkedList.java
@@ -0,0 +1,5 @@
+package Microsoft.LinkedLists;
+//https://leetcode.com/explore/interview/card/microsoft/32/linked-list/169/
+
+public class ReverseLinkedList {
+}
