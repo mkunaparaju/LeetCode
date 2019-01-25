17f27de5a82aefc43259c9c8323f46b6ceb7f64a
Manasa Kunaparaju <mkunaparaju@gmail.com>
2019-01-25 07:18:00 -0700

    started trees

--- Google/DynamicProgramming/VacationDaysDp.java
+++ Google/DynamicProgramming/VacationDaysDp.java
@@ -1,42 +1,72 @@
 package Google.DynamicProgramming;
 
+import java.util.Arrays;
+
 public class VacationDaysDp {
 
     public static void main(String[] args) {
-        int [][] flights = {{0,1,1},{1,0,1},{1,1,0}}, days = {{1,3,1},{6,0,3},{3,3,3}};
-        int [][] flights2 = {{0,0,0},{0,0,0},{0,0,0}}, days2 = {{1,1,1},{7,7,7},{7,7,7}};
-        int [][] flights3 = {{0,1,1},{1,0,1},{1,1,0}}, days3 = {{7,0,0},{0,7,7},{7,7,0}};
-        int [][] flights4 = {{0,1,0},{0,0,0},{0,0,0}}, days4 = {{0,0,7},{2,0,0},{7,7,7}};
+        int[][] flights = {{0, 1, 1}, {1, 0, 1}, {1, 1, 0}}, days = {{1, 3, 1}, {6, 0, 3}, {3, 3, 3}};
+        int[][] flights2 = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}, days2 = {{1, 1, 1}, {7, 7, 7}, {7, 7, 7}};
+        int[][] flights3 = {{0, 1, 1}, {1, 0, 1}, {1, 1, 0}}, days3 = {{7, 0, 0}, {0, 7, 7}, {7, 7, 0}};
+        int[][] flights4 = {{0, 1, 0}, {0, 0, 0}, {0, 0, 0}}, days4 = {{0, 0, 7}, {2, 0, 0}, {7, 7, 7}};
 
         VacationDaysDp vd = new VacationDaysDp();
-        System.out.println(vd.maxVacationDays(flights3, days3));
+        System.out.println(vd.maxVacationDaysDp(flights, days));
 
     }
 
     public int maxVacationDays(int[][] flights, int[][] days) {
 
-        return maxVacationDays(flights,days, 0, 0);
+        int[][] memo = new int[days[0].length][days.length];
+        for (int i = 0; i < memo.length; i++) {
+            Arrays.fill(memo[i], -1);
+        }
+        return maxVacationDays(flights, days, 0, 0, memo);
     }
 
-    private int maxVacationDays(int[][] flights, int[][] days, int week, int currentCity) {
-        if(week == days[0].length) return 0;
-
+    private int maxVacationDays(int[][] flights, int[][] days, int week, int currentCity, int[][] memo) {
+        if (week == days[0].length) return 0;
+        if (memo[week][currentCity] != -1) return memo[week][currentCity];
         int maxVacay = 0;
 
-        for(int i = 0; i < days.length; i++){
-            if(flights[currentCity][i] != 0) {
-                int vacayDays = days[i][week] + maxVacationDays(flights, days, week+1, i);
+        for (int i = 0; i < days.length; i++) {
+            if (flights[currentCity][i] != 0) {
+                int vacayDays = days[i][week] + maxVacationDays(flights, days, week + 1, i, memo);
                 maxVacay = Math.max(maxVacay, vacayDays);
 
-                System.out.println("current city " + currentCity + " week " +week + " i " + i + " vacay " + vacayDays   );
-            }
-            else if(currentCity == i){
-                int vacayDays = days[currentCity][week] + maxVacationDays(flights, days, week+1, currentCity);
+//                System.out.println("current city " + currentCity + " week " +week + " i " + i + " vacay " + vacayDays   );
+            } else if (currentCity == i) {
+                int vacayDays = days[currentCity][week] + maxVacationDays(flights, days, week + 1, currentCity, memo);
                 maxVacay = Math.max(maxVacay, vacayDays);
-                System.out.println("week " +week + " i " + i + " vacay " + vacayDays   );
+//                System.out.println("week " +week + " i " + i + " vacay " + vacayDays   );
             }
         }
+        memo[week][currentCity] = maxVacay;
         return maxVacay;
+    }
+
+    private int maxVacationDaysDp(int[][] flights, int[][] days) {
+        int[] dp = new int[days.length];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+
+        for (int i = 0; i < days[0].length; i++) {
+            int[] temp = new int[dp.length];
+            Arrays.fill(temp, -1);
+            for (int j = 0; j < days.length; j++) {
+                for (int k = 0; k < days.length; k++) {
+                    if (flights[j][k] == 1 || j == k) {
+                        temp[j] = Math.max(temp[j], dp[k] + days[j][i]);
+                    }
+                }
+            }
+            dp = temp;
+        }
+        int max = 0;
+        for (int m : dp) {
+            max = Math.max(m, max);
+        }
+        return max;
     }
 
 
--- Google/TreesGraphs/EvaluateDivision.java
+++ Google/TreesGraphs/EvaluateDivision.java
@@ -1,14 +1,81 @@
 package Google.TreesGraphs;
 
+import sun.security.krb5.internal.PAData;
+
+import java.util.ArrayDeque;
+import java.util.HashMap;
+
 public class EvaluateDivision {
 
     public static void main(String[] args) {
     }
 
     public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
-
+        HashMap<String, Node> nodeMap = new HashMap<>();
         double [] answers = new double[queries.length];
+
+        for(int i = 0; i < equations.length ; i++){
+            String [] equation = equations[i];
+            String parent = equation[0];
+            String child = equation[1];
+
+            Node parentNode = null;
+            Node childNode = null;
+
+            if(!nodeMap.containsKey(parent)) {
+                parentNode = new Node(parent);
+                nodeMap.put(parent, parentNode);
+            } else {
+                parentNode = nodeMap.get(parent);
+            }
+
+            if(!nodeMap.containsKey(child)){
+                childNode = new Node(child);
+                nodeMap.put(child, childNode);
+            } else {
+                childNode = nodeMap.get(child);
+            }
+
+            parentNode.addChild(childNode, values[i]);
+            childNode.addParent(parentNode, 1/values[i]);
+        }
+
+        for(int i = 0; i < queries.length; i++){
+            int pathValue = getPathVal(queries[i][0],queries[i][1], nodeMap);
+        }
 
         return answers;
     }
+
+    private int getPathVal(String pQuery, String cQuery, HashMap<String, Node> nodeMap) {
+        ArrayDeque childStack = new ArrayDeque();
+
+        
+    }
+}
+
+
+class Node{
+    private String label;
+    private HashMap<String, Double> childMap;
+    private HashMap<String, Double> parentMap;
+
+    Node(String lable){
+        this.label = lable;
+        childMap =new HashMap<>();
+        parentMap = new HashMap<>();
+    }
+
+    void addChild(Node child, double weight){
+       if( !childMap.containsKey(child.label)) {
+            childMap.put(child.label, weight);
+        }
+    }
+
+    void addParent(Node parent, double weight){
+        if(!parentMap.containsKey(parent.label)){
+            parentMap.put(parent.label, weight);
+        }
+    }
+
 }
