de538974579f0155b5267ff7af11f975d715dd84
Manasa Kunaparaju <mkunaparaju@gmail.com>
2019-02-14 06:34:04 -0700

    update

--- Microsoft/TreesAndGraphs/LowestCommonAncestor.java
+++ Microsoft/TreesAndGraphs/LowestCommonAncestor.java
@@ -2,11 +2,5 @@ package Microsoft.TreesAndGraphs;
 
 public class LowestCommonAncestor {
 
-    public static void main(String[] args) {
 
-    }
-
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
-    }
 }
