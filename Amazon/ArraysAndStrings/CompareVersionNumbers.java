f258fb4ddf185882d20bc9d0ddf358cbefc9cefa
Manasa Kunaparaju <mkunaparaju@gmail.com>
2019-04-18 07:45:27 -0600

    update

--- Amazon/ArraysAndStrings/CompareVersionNumbers.java
+++ Amazon/ArraysAndStrings/CompareVersionNumbers.java
@@ -1,45 +1,74 @@
 package Amazon.ArraysAndStrings;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.ListResourceBundle;
-
 public class CompareVersionNumbers {
     public static void main(String[] args) {
 
         String version1 = "1.01", version2 = "1.001";
         String version3 = ".01", version4 = "1.01";
+
         CompareVersionNumbers cvn = new CompareVersionNumbers();
-        cvn.compareVersion(version3, version4);
+//        System.out.println(cvn.compareVersion(version1, version2));
+        System.out.println(cvn.compareVersion("1.0", "1.0.0"));
     }
 
     public int compareVersion(String version1, String version2) {
 
-        List<String> ver1 = Arrays.asList(version1.split("\\."));
-        List<String> ver2 = Arrays.asList(version2.split("\\."));
+//        System.out.println("str1 " + version1 + " str2 "+ version2);
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
 
-        int l1 = ver1.size();
-        int l2 = ver2.size();
+        StringBuilder s1 = new StringBuilder();
+        StringBuilder s2 = new StringBuilder();
 
-        if(l1< l2) {
-            int padding = l2 - l1;
-            for(int i = 0; i < padding; i++){
-                ver1.add("0");
-            }
-        }else if (l2< l1) {
-            int padding = l1 - l2;
-            for(int i = 0; i < padding; i++){
-                ver1.add("0");
-            }
+        for (int i = 0; i < v1.length; i++) {
+            s1.append(v1[i]);
         }
 
-        for(int i = 0 ; i< l1; i++){
-            System.out.print(ver1.get(i));
-            System.out.println();
-            System.out.print(ver2.get(i));
+        for (int i = 0; i < v2.length; i++) {
+            s2.append(v2[i]);
+        }
+        int length = 0;
+
+//        System.out.println("s1 " + s1);
+//        System.out.println("s2 " + s2);
+
+        int l1 = s1.length();
+        int l2 = s2.length();
+
+        if (l1 < l2) {
+            length = l1;
+        } else {
+            length = l2;
         }
 
+//        System.out.println("l1 " + l1 + " l2 " + l2);
+
+        for (int i = 0; i <= length; i++) {
+
+            System.out.println("s1 " + s1.charAt(i));
+            System.out.println("s2 " + s2.charAt(i));
+
+            if (i == length) {
+                if (l1 == length) {
+                    int lenRem = l2 - length;
+                    for (int j = 0; j <= lenRem; j++) {
+                        if (s2.charAt(i + j) > 0) return 1;
+                        if(i+j == l2 -1 && s2.charAt(i+j) ==0) return 0;
+                    }
+                } else if (l2 == length) {
+                    int lenRem = l1 - length;
+                    for (int j = 0; j <= lenRem; j++) {
+                        if (s1.charAt(i + j) > 0) return -1;
+                    }
+                }
+            }
+//            int x = ('1' > '0')? 1: 0;
+//            System.out.println("x " + x);
+                if (s1.charAt(i) > s2.charAt(i)) return -1;
+                if (s1.charAt(i) < s2.charAt(i)) return 1;
+
+
+        }
         return 0;
     }
-
 }
